e335593f0ea16abf6c551645a40b21e88eed96a9
Natshark <modin.ru2@mail.ru>
2024-05-19 23:31:17 +0500

    lab 2 finale
    Всё сделано

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,26 +2,95 @@
 
 namespace App\Http\Controllers;
 
+use App\DTO\UserResource;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class UserController extends Controller
 {
-    public function getPhpVersion()
+    public function login(LoginRequest $request)
     {
-        $phpVersion = phpversion();
-        return response()->json(['php_version' => $phpVersion]);
+        $this->deleteExpiredTokens();
+        $loginDTO = $request->toDTO();
+
+        if (Auth::attempt(['username' => $loginDTO->username, 'password' => $loginDTO->password]))
+        {
+            $user = Auth::user();
+
+            $activeTokensCount = $user->tokens()->count();
+            $maxActiveTokens = env('MAX_ACTIVE_TOKENS_PER_USER');
+
+            if ($activeTokensCount < $maxActiveTokens)
+            {
+                $token = $user->createToken($loginDTO->username . '_token', [], now()
+                    ->addMinutes(env('SANCTUM_TOKEN_EXPIRATION')))->plainTextToken;
+                return response()->json(['token' => $token], 200);
+            }
+
+            return response()->json(['error' => 'Превышено максимальное количество активных токенов']);
+        }
+
+        return response()->json(['error' => 'Неверный логин или пароль']);
     }
 
-    public function getClientInfo(Request $request)
+    public function register(RegisterRequest $request): JsonResponse
     {
-        $ip = $request -> ip();
-        $userAgent = $request->header('User-Agent');
-        return response()->json(['ip' => $ip, 'useragent' => $userAgent]);
+        $registerDTO = $request->toDTO();
+
+        $user = new User([
+            'username' => $registerDTO->username,
+            'email' => $registerDTO->email,
+            'password' => $registerDTO->password,
+            'birthday' => $registerDTO->birthday,
+        ]);
+
+        $user->save();
+
+        return response()->json(['Экземпляр ресурса созданного пользователя' => $user], 201);
     }
 
-    public function getDatabaseInfo()
+    public function getUser(): JsonResponse
     {
-        $databaseInfo = config('database.connections.' . config('database.default'));
-        return response()->json(['database_info' => $databaseInfo], status:200);
+        $user = Auth::user();
+        $user = new UserResource(['username' => $user->username, 'email' => $user->email,
+            'password' => $user->password, 'birthday' => $user->birthday]);
+        return response()->json(['Пользователь' => $user]);
+    }
+
+    public function logout()
+    {
+        $user = Auth::user();
+
+        if ($user)
+        {
+            $user->currentAccessToken()->delete();
+
+            return response()->json(['message' => 'Вы успешно разлогинились']);
+        }
+
+        return response()->json(['error' => 'Вы не авторизованы']);
+    }
+
+    public function getTokens()
+    {
+        $this->deleteExpiredTokens();
+        return response()->json(['tokens' => Auth::user()->tokens->pluck('token')]);
+    }
+
+    public function logoutAll()
+    {
+        Auth::user()->tokens()->delete();
+        return response()->json(['message' => 'Все ваши токены отозваны'], 200);
+    }
+
+    public function deleteExpiredTokens()
+    {
+        PersonalAccessToken::where('expires_at', '<', now())->delete();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [UserController::class, 'login'])->name('login');
+    Route::middleware('guest:sanctum')->post('/register', [UserController::class, 'register']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/me', [UserController::class, 'getUser'])->name('me');
+        Route::post('/out', [UserController::class, 'logout']);
+        Route::get('/tokens', [UserController::class, 'getTokens']);
+        Route::post('/out_all', [UserController::class, 'logoutAll']);
+    });
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
